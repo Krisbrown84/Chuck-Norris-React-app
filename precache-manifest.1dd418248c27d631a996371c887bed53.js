ed48aea487ef1825fb05d4c28e200c2cf6f3c6e7
Kris Brown <mouserbrown@googlemail.com>
2020-04-03 10:51:12 +0100

    Updates

---
+++ precache-manifest.1dd418248c27d631a996371c887bed53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96ab99cf51ab106cc9e918253623bac",
+    "url": "/Chuck-Norris-React-app/index.html"
+  },
+  {
+    "revision": "55847cdbfbec5b147b5f",
+    "url": "/Chuck-Norris-React-app/static/css/main.f5eb9b75.chunk.css"
+  },
+  {
+    "revision": "1709be43ef7ed4a35f7e",
+    "url": "/Chuck-Norris-React-app/static/js/2.2e73d799.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chuck-Norris-React-app/static/js/2.2e73d799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55847cdbfbec5b147b5f",
+    "url": "/Chuck-Norris-React-app/static/js/main.73160f13.chunk.js"
+  },
+  {
+    "revision": "b926b73c360b3fba4bb4",
+    "url": "/Chuck-Norris-React-app/static/js/runtime-main.7511f28f.js"
+  },
+  {
+    "revision": "173d39ce837b64c1679deae0b7fa9c4c",
+    "url": "/Chuck-Norris-React-app/static/media/Chuck-norris.173d39ce.jpg"
+  },
+  {
+    "revision": "98b26e871a0da6c0ea9d91eab8665f02",
+    "url": "/Chuck-Norris-React-app/static/media/chuck-norris.98b26e87.png"
+  }
+]);
