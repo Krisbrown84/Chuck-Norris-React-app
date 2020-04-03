d90270bd34abc7f1f11d9bbdb5111d63ea1a4f74
Kris Brown <mouserbrown@googlemail.com>
2020-04-03 10:50:26 +0100

    deploy my react app to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import NorrisPic from "./Chuck-norris.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+
+  state = {
+    jokes: [],
+    loading: false
+  };
+
+  componentDidMount = async () => {
+    this.setState({
+      loading: true
+    });
+  
+    const response = await axios.get(
+      "https://api.chucknorris.io/jokes/random"
+    );
+
+    this.setState({
+      jokes: response.data.value,
+      loading: false
+    });
+  }
+
+  
+
+  render () {
+    return (
+      <div>
+        <h1> Random Chuck Norris Jokes </h1>
+        <img src={NorrisPic} alt="NorrisPic" />
+        {/* <NorrisPic loading={this.state.loading} /> */}
+        <button onClick={this.componentDidMount}>Generate Random Joke</button>
+        <h2>{this.state.jokes}</h2>
+      </div>
+    );
+  }
+};
 
 export default App;
